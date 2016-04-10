145af61eeb2dcee1ec32abba0b9fb5868443c431
karataev <eugene.karataev@gmail.com>
2016-04-11 01:39:27 +0600

    Count solution

--- app/count.js
+++ app/count.js
@@ -2,6 +2,23 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.countAnswers =  {
   count : function (start, end) {
+    var current;
+    var interval;
 
+    current = start;
+    console.log(current);
+    interval = setInterval(function () {
+      current++;
+      console.log(current);
+      if (current >= end) {
+        clearInterval(interval);
+      }
+    }, 100);
+
+    this.cancel = function() {
+      clearInterval(interval);
+    };
+
+    return this;
   }
 };
