c42c9d803aad4141971baf0ba7646c24917b3635
karataev <eugene.karataev@gmail.com>
2016-04-11 01:17:06 +0600

    Async answers

--- app/async.js
+++ app/async.js
@@ -2,10 +2,26 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.asyncAnswers = {
   async : function(value) {
-
+    return new Promise(function (resolve, reject) {
+      resolve(value);
+    });
   },
 
   manipulateRemoteData : function(url) {
+    var p = new Promise(function (resolve, reject) {
+      $.ajax({
+          url: url
+        })
+        .done(function (res) {
+          var names = res.people.map(function (x) {
+            return x.name;
+          });
+
+          resolve(names.sort());
+        })
+    })
+
+    return p;
 
   }
 };
