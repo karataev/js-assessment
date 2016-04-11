42dc2ab818c61bf0626120d6f8a3523af38ce91b
karataev <eugene.karataev@gmail.com>
2016-04-11 22:52:52 +0600

    FlowControl solution

--- app/flowControl.js
+++ app/flowControl.js
@@ -11,5 +11,12 @@ exports.flowControlAnswers = {
     // otherwise the function should return the number, or false if no number
     // was provided or the value provided is not a number
 
+    if (typeof num != 'number') return false;
+
+    var res = '';
+    if (num % 3 === 0) res += 'fizz';
+    if (num % 5 === 0) res += 'buzz';
+
+    return res === '' ? num : res;
   }
 };
