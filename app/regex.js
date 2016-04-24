8417d368bc8b784d9429b5f18c4aa4f5eced8c9d
karataev <eugene.karataev@gmail.com>
2016-04-24 21:45:38 +0600

    Solutions for RegExps

--- app/regex.js
+++ app/regex.js
@@ -2,25 +2,26 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.regexAnswers = {
   containsNumber : function(str) {
-
+    return str.search(/\d/) >= 0;
   },
 
   containsRepeatingLetter : function(str) {
-
+    return /([a-zA-Z])\1/.test(str);
   },
 
   endsWithVowel : function(str) {
-
+    return /[aeiou]$/i.test(str);
   },
 
   captureThreeNumbers : function(str) {
-
+    var res = /\d{3}/.exec(str);
+    return res ? res[0] : false;
   },
 
   matchesPattern : function(str) {
-
+    return /^\d{3}-\d{3}-\d{4}$/.test(str);
   },
   isUSD : function(str) {
-
+    return /^\$\d+(,\d{3})*(\.\d{2})?$/.test(str);
   }
 };
